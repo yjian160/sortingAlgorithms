1cddaf89e68929e4c8f6d9c0118f47ff7516b2c0
Jian Yeung <yeungjian@gmail.com>
2019-10-17 21:57:18 -0700

    fixed insertion sort bug

--- insertionSort.js
+++ insertionSort.js
@@ -3,11 +3,11 @@ const insertionSort = (arr) => {
   for (var i = 1; i < arr.length; i++) {
     var currentVal = arr[i];
     var j = i - 1;
-    while(j >= 0 && arr[j] > current[i]) {
+    while(j >= 0 && arr[j] > currentVal) {
       arr[j+1] = arr[j];
       j--; 
     }
-    arr[j] = currentVal;
+    arr[j+1] = currentVal;
   }
 
   return arr;
